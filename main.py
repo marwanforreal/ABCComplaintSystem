1d8cc02e916b09893205c5c1405781953d6fa431
marwanforreal <69703434+marwanforreal@users.noreply.github.com>
2021-06-23 19:44:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ABC.sqlite3'
 db = SQLAlchemy(app)
 
 
-class Users(db.Model): #Users Table in Database
+class Users(db.Model):
     _id = db.Column("ID", db.Integer, primary_key=True)
     _firstName = db.Column("FirstName", db.String(100))
     _lastname = db.Column("LastName", db.String(100))
     _username = db.Column("UserName", db.String(100))
     _Password = db.Column("Password", db.String(100))
     _Email = db.Column("Email", db.String(100))
-    _role = db.Column("Role", db.String(100)) #to differentiate between admin users and normal users depending on the signup page 
+    _role = db.Column("Role", db.String(100))
     _complaints = db.relationship('Complaints', backref='ComplaintWriter', lazy='dynamic')
 
-#Note: there's a 1 to many relationship between the table above and the table below. 
 
-class Complaints(db.Model): #Complains Table in DataBase 
+
+class Complaints(db.Model):
     _id = db.Column("ComplaintID", db.Integer, primary_key=True)
     _UserID = db.Column("UserID", db.Integer, db.ForeignKey('users.ID'))
     _title = db.Column("Title", db.String(30))
@@ -52,13 +52,13 @@ def SignInPage():
 
         loggedInUser = Users.query.filter_by(_username=userNameLoginInput).first()
 
-        if loggedInUser != None: #if the username was found 
-            session['LoggedInUser'] = loggedInUser._username #starting a session 
-        if loggedInUser == None: #if the username wasn't found
+        if loggedInUser != None:
+            session['LoggedInUser'] = loggedInUser._username
+        if loggedInUser == None:
             return "Wrong User Name"
         if not loggedInUser or not check_password_hash(loggedInUser._Password, passwordLoginInput):
-            return "Wrong Password" #if the password is wrong, could work without the first condition too (not loggedInUser)
-        if loggedInUser._role == "admin": #if the Users role is admin, it will take them to the admin page 
+            return "Wrong Password"
+        if loggedInUser._role == "admin":
             return redirect(url_for('AdminPage'))
         else:
             return redirect(url_for("UserHomePage", user=loggedInUser._username))
@@ -70,7 +70,7 @@ def SignUp():
         return render_template("SignUpHTMLPage.html")
     else:  # method.request == "POST"
         userNameDuplicationFlag = Users.query.filter_by(_username=request.form['username']).first()
-        if userNameDuplicationFlag == None: #if the username existed in the database it wouldn't return none 
+        if userNameDuplicationFlag == None:
             UserObject = Users(_firstName=request.form['fname'],
                                _lastname=request.form['lname'],
                                _username=request.form['username'],
@@ -93,7 +93,7 @@ def UserHomePage(user):  # THIS IS WHERE YOU SEND COMPLAINTS
                                      _Category=request.form['category'],
                                      _ComplaintMessage=request.form['complaint'],
                                      ComplaintWriter=complainingUserObject,
-                                     _complaintStatus='Pending Resolution') #Creating a complaint object which references the user signed in by the session they're in 
+                                     _complaintStatus='Pending Resolution')
 
         db.session.add(ComplaintObject)
         db.session.commit()
@@ -109,7 +109,7 @@ def TicketStatus():
         CurrentUserID = complainingUserObject._id
         ComplaintObjects = Complaints.query.filter_by(_UserID=CurrentUserID).all()  # returns a list of objects
 
-        for x in range(len(ComplaintObjects)):  # To transform the list of objects to a list of strings that can be itereated in the html file 
+        for x in range(len(ComplaintObjects)):  # To transform the list of objects to a list of strings that can be used in html
             ListOfComplaintTitles.append(ComplaintObjects[x]._title)
             ListOfComplaintStatus.append(ComplaintObjects[x]._complaintStatus)
 
@@ -124,12 +124,12 @@ def AdminPage():
     if request.method=='GET':
         AllComplaintsTitles = []
         AllComplaintsStatuses = []
-        AdminUserObject = Users.query.filter_by(_username=session['LoggedInUser']).first() #this could be a User Object too if someone accessed the page which would trigger the if statement below 
+        AdminUserObject = Users.query.filter_by(_username=session['LoggedInUser']).first()
 
         if AdminUserObject._role == "admin":
             AllComplaints = Complaints.query.all()  # returns a list of objects
         else:
-            return "Not Authorized" #if a non admin user tried to access this page 
+            return "Not Authorized"
 
         for x in range(len(AllComplaints)):  # To transform the list of objects to a list of strings that can be used in html
             AllComplaintsTitles.append(AllComplaints[x]._ComplaintMessage)
@@ -144,14 +144,14 @@ def AdminPage():
         ComplaintObject = Complaints.query.get(ComplaintID)
         if ComplaintObject != None:
             ComplaintObject._complaintStatus = NewStatus
-            db.session.commit() #committing the new status to the complaint with the chosen id 
+            db.session.commit()
         else:
-            return "Wrong Id" #if the admin entered an ID that doesn't exist 
+            return "Wrong Id"
         return "thanks"
 
 
-@app.route("/SignUpAdmin", methods=['GET', 'POST']) #while using this function it is better to make sure the database already has an admin member before starting the code, it can be either done manually or via the constructor 
-def SignUpAdmin(): #this page can only be accessed by admins 
+@app.route("/SignUpAdmin", methods=['GET', 'POST'])
+def SignUpAdmin():
     UserObject = Users.query.filter_by(_username=session['LoggedInUser']).first()
 
     if UserObject._role != "admin": #If a normal User tried to sign up an Admin
@@ -169,7 +169,7 @@ def SignUpAdmin(): #this page can only be accessed by admins
             db.session.commit()
             return "thank you"
         else:
-            return "Username Duplication" 
+            return "Username Duplication"
 
 
 if __name__ == '__main__':
